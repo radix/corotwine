5678ab466c1837c70f41f7641be96754ef302159
Christopher Armstrong <radix@twistedmatrix.com>
2008-03-29 18:04:09 -0400

    Hooray a setup.py script

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os, sys
+
+from distutils.core import setup
+
+
+def find_packages():
+    # implement a simple find_packages so we don't have to depend on
+    # setuptools
+    packages = []
+    for directory, subdirectories, files in os.walk("corotwine"):
+        if '__init__.py' in files:
+            packages.append(directory.replace(os.sep, '.'))
+    return packages
+
+
+setup_args = dict(
+    name='Corotwine',
+    version='0.1',
+    license="MIT",
+    description='Coroutines for Twisted',
+    author='Christopher Armstrong',
+    author_email='radix@twistedmatrix.com',
+    url='http://twistedmatrix.com/users/radix/corotwine/',
+    packages=find_packages(),
+    )
+
+if 'setuptools' in sys.modules:
+    setup_args["install_requires"] = "Twisted>=8.0.0"
+
+
+if __name__ == '__main__':
+    setup(**setup_args)
